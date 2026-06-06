5ec74c20338c12889871701cce48b101d6aae0db
ImmShark <txt6485@gmail.com>
2026-06-06 22:07:37 +0700

    up bot

--- messages1.js
+++ messages1.js
@@ -153,22 +153,22 @@ module.exports = {
           "https://media.discordapp.net/attachments/1160008472893603871/1512111182713065472/endd.png?ex=6a22e6c7&is=6a219547&hm=5ec9b4603fc3df7c33709ce1a70c1ef6f836df1ab7abb74aa2e7b59387bb0f05&=&format=webp&quality=lossless&width=1860&height=283",
         fields: [
           {
-            name: ":anstiff_boost: 14x Boost Server Lv3 4 Tháng:",
+            name: "<:21366galaxystore:1512143059243241552> 14x Boost Server Lv3 4 Tháng:",
             value: "```PRICE: 500.000 VNĐ (BH 3 THÁNG)```",
             inline: false,
           },
                     {
-            name: ":anstiff_boost: 28x Boost Server Lv3 4 Tháng:",
+            name: "<:21366galaxystore:1512143059243241552> 28x Boost Server Lv3 4 Tháng:",
             value: "```PRICE: 900.000 VNĐ (BH 3 THÁNG)```",
             inline: false,
           },
           {
-            name: ":anstiff_boost: Lẻ 2x Đến 10x Bust Server Lv3 4 Tháng (75k/2x):",
+            name: "<:21366galaxystore:1512143059243241552> Lẻ 2x Đến 10x Bust Server Lv3 4 Tháng (75k/2x):",
             value: "```PRICE: 90.000 VNĐ (2 B00ST)```",
             inline: false,
           },
           {
-            name: ":anstiff_boost: 14x Boost Server Lv3 1 Tháng:",
+            name: "<:21366galaxystore:1512143059243241552> 14x Boost Server Lv3 1 Tháng:",
             value: "```PRICE: 200.000 VNĐ (BH 1 THÁNG)```",
             inline: false,
           },
