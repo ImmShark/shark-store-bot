f15b296953d2e326c0e1e5f52cc7dbf04f45883a
ImmShark <txt6485@gmail.com>
2026-06-13 16:45:33 +0700

    fix netflix

--- qr.js
+++ qr.js
@@ -16,10 +16,10 @@ module.exports = {
         "https://media.discordapp.net/attachments/1161326028682170489/1512350783511855144/635002420_2099748023930273_8969084125919827184_n.jpg?ex=6a23c5ec&is=6a22746c&hm=53d4547240c6cbff44df18fdca6fb5c5dafd24a2de80745169a068166c05c768&=&format=webp&width=930&height=930"
       ) // QR code
       .addFields(
-        { name: "🏦 Ngân Hàng:", value: "```MB BANK```", inline: true },
-        { name: "💳 Số Tài Khoản:", value: "```0328206839```", inline: true },
-        { name: "👤 Chủ Tài Khoản:", value: "```LE HOANG VU```", inline: true },
-        { name: "📝 Nội Dung:", value: "```SHARKSTORE```", inline: true }
+        { name: "<:3339shinystar2:1512142205094068367> Ngân Hàng:", value: "```MB BANK```", inline: true },
+        { name: "<:1661shinystar6:1512141829758386226> Số Tài Khoản:", value: "```0328206839```", inline: true },
+        { name: "<:3629shinystar4:1512142399932076223> Chủ Tài Khoản:", value: "```LE HOANG VU```", inline: true },
+        { name: "<:8819shinystar3:1512142936287088791> Nội Dung:", value: "```SHARKSTORE```", inline: true }
       )
       .setFooter({
   text: "***VUI LÒNG GỬI BILL VÀO TICKET KHI ĐÃ CHUYỂN KHOẢN***"
